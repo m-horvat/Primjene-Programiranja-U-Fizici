e4956aaeb9d891e7f9ee2219c22b207f155e17e4
m-horvat <mhorvat@pmfst.hr>
2025-05-05 15:51:31 +0200

    problem 6 almost complete

--- Vježbe/Vježba 6/harmonic_oscillator.py
+++ Vježbe/Vježba 6/harmonic_oscillator.py
@@ -7,22 +7,22 @@ class HarmonicOscillator:
         self.startingpoint = x0
         self.startingspeed = v0
 
-    def __move(self,x0,v0):
+    def __move(self,x0,v0,dt):
         a = -(self.constant*x0)/self.mass
-        v = v0 + a*self.deltat
-        x = x0 + v*self.deltat
+        v = v0 + a*dt
+        x = x0 + v*dt
 
         return(x,v,a)
     
-    def long_movement(self):
+    def long_movement(self,dt):
         x0 = self.startingpoint
         v0 = self.startingspeed
         a_list = []
         v_list = []
         x_list = []
-        cycles = self.time/self.deltat
+        cycles = self.time/dt
         for idx in range(0,int(cycles)):
-            movement = self.__move(x0,v0)
+            movement = self.__move(x0,v0,dt)
             x_list.append(movement[0])
             v_list.append(movement[1])
             a_list.append(movement[2])
@@ -30,28 +30,79 @@ class HarmonicOscillator:
             v0 = v_list[idx]
         return(x_list,v_list,a_list)
     
-    def plotting(self,t,dt):
+    def __analyt_sol_displacement(self,time):
+        import numpy as np
+        omega = np.sqrt(self.constant/self.mass)
+        A = self.startingpoint
+        result = []
+        for i in time:
+            x = A*np.cos(omega*i)
+            result.append(x)
+        return(result)
+    
+    def __analyt_sol_period(self):
+        import numpy as np
+        T = 2*np.pi*np.sqrt(self.mass/self.constant)
+        return(T)
+
+    def plotting(self,t):
         import matplotlib.pyplot as matplot
         import numpy as np
 
-        time = np.linspace(0,t,int(t/dt))
+        time = np.linspace(0,t,int(t/self.deltat))
+        lists = self.long_movement(dt)
+        comparison = self.__analyt_sol_displacement(time)
 
-        lists = test.long_movement()
-        matplot.plot(time,lists[0],label='at')
-        matplot.plot(time,lists[1],label='vt')
-        matplot.plot(time,lists[2],label='xt')
-        matplot.legend()
+        fig, axes = matplot.subplots(nrows=1, ncols=3)
+
+        axes[0].plot(time,comparison,label='analytical solution')
+        axes[0].plot(time,lists[0],'ro',markersize=1,label='xt')
+        axes[1].plot(time,lists[1],'go',markersize=1,label='vt')
+        axes[2].plot(time,lists[2],'bo',markersize=1,label='at')
         matplot.show()
 
+    def multiple_dt(self,t,dt1,dt2,dt3):
+        import matplotlib.pyplot as matplot
+        import numpy as np
+
+        dt_list = []
+        multi_dt = [dt1,dt2,dt3]
+
+        for i in multi_dt:
+            Ndt = i
+            dt_list.append(self.long_movement(Ndt)[0])
+
+        for idx,i in enumerate(dt_list):
+            time = np.linspace(0,t,int(t/multi_dt[idx]))
+            comparison = self.__analyt_sol_displacement(time)
+            matplot.plot(time,i,'ro',markersize=0.5,label='dt = {}'.format(multi_dt[idx]))
+        matplot.plot(time,comparison,label='analytical solution')
+        matplot.show()
+
+
+    def period_titranja(self):
+        
+        result = 2
+        actual_result = self.__analyt_sol_period()
+        print('Period titranja numerički je {}, dok je stvarni rezultat {}'.format(result,actual_result))
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 m = 1
 k = 10
 t = 7
-x0 = 2
-dt = 0.001
+x0 = 5
+v0 = 0
+dt = 0.01
 
-test = HarmonicOscillator(m,k,t,x0,0,dt)
-test.plotting(t,dt)
+dt1 = 0.001
+dt2 = 0.01
+dt3 = 0.05
+
+test = HarmonicOscillator(m,k,t,x0,v0,dt)
+test.plotting(t)
+test.period_titranja()
+test.multiple_dt(t,dt1,dt2,dt3)
 
 
 
