dce468e42906aad37836933b94b4033fbf3d1f0d
m-horvat <mhorvat@pmfst.hr>
2025-05-07 23:14:50 +0200

    problem 5 fully finished

--- Vježbe/Vježba 5/calculus.py
+++ Vježbe/Vježba 5/calculus.py
@@ -5,12 +5,12 @@ def derivacija(function,x,delta=0.01,O='three-step'):
         deriv = (function(x+delta)-function(x-delta))/(2*delta)
     return deriv
 
-def derivacija_raspon(function,xL,xH,delta=0.01):
+def derivacija_raspon(function,xL,xH,delta=0.01,O='three-step'):
     import numpy as np
     dots = np.linspace(xL,xH)
     deriv_dots = []
     for i in dots:
-        deriv = derivacija(function,i,delta)
+        deriv = derivacija(function,i,delta,O)
         deriv_dots.append(deriv)
     return dots,deriv_dots
     
@@ -21,10 +21,9 @@ def integracija(function,xL,xH,N):
     dots = np.linspace(xL,xH,N)
     donj_med = 0
     gornj_med = 0
+    diff = dots[2]-dots[1]
     for idx,i in enumerate(dots):
-        if i == dots[-1]:
-            break
-        integ_dio = function(dots[idx])*(dots[idx+1]-i)
+        integ_dio = function(dots[idx])*diff
         if idx != 0:
             gornj_med += integ_dio
         if idx != N-1:
@@ -34,7 +33,6 @@ def integracija(function,xL,xH,N):
 def trapezna_integracija(function,xL,xH,N):
     import numpy as np
     dots = np.linspace(xL,xH,N)
-    print(dots[-1], dots[0])
     integral = 0
     delta_x = (xH-xL)/N
     for idx,i in enumerate(dots):
--- Vježbe/Vježba 5/zadatak2_testiranje_integracija.py
+++ Vježbe/Vježba 5/zadatak2_testiranje_integracija.py
@@ -8,15 +8,37 @@ def f_quad(x):
 
 # Raspon točaka od A do B
 a = -2
-b = 2
-N = 500
-T = np.linspace(a,b)
+b = 5
+Nd = 100
+Nu = 200
+N = [x for x in range(Nd,Nu)]
 
-reg_integ = (calc.integracija(f_quad,a,b,N))
-trapez_integ =(calc.trapezna_integracija(f_quad,a,b,N))
+reg_integ_list_g = []
+reg_integ_list_d = []
+trap_integ_list = []
+
+for i in N:
+    x = calc.integracija(f_quad,a,b,i)
+    reg_integ_list_d.append(x[0])
+    reg_integ_list_g.append(x[1])
+    trap_integ_list.append(calc.trapezna_integracija(f_quad,a,b,i))
+
+# - - - - - - - - - - - - - -
+# Stvaranje analitičkog rješenja
+
+def F_quad(x):
+    return 2/3*x**3+3*x
+
+analytical_solution = F_quad(b)-F_quad(a)
+
+# - - - - - - - - - - - - - -
 
 fig, axes = matplot.subplots(nrows=1, ncols=2)
 
-axes[0].plot(N,reg_integ[1],'ro',markersize=1)
-axes[1].plot(N,trapez_integ[1],'ro',markersize=1)
+axes[0].plot(N,reg_integ_list_d,'ro',markersize=1,label='Donja granica integracije')
+axes[0].plot(N,reg_integ_list_g,'bo',markersize=1,label='Gornja granica integracije')
+axes[0].axhline(analytical_solution,color='#BBBBBB',linestyle='dotted')
+axes[1].plot(N,trap_integ_list,'ro',markersize=1,label='Trapezna integracija')
+axes[1].axhline(analytical_solution,color='#BBBBBB',linestyle='dotted')
+matplot.legend()
 matplot.show()
